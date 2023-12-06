e896c9e2e0c721052af7bb4c1dfceec27ea4e2cb
Root@BlackOrchid <rootblackorchid@ip-192-168-211-113.ap-southeast-1.compute.internal>
2023-12-06 16:45:59 +0800

    Removed ScrollTopAndComment component

--- app/chatbot/page.tsx
+++ app/chatbot/page.tsx
@@ -81,7 +81,7 @@ export default function ChatbotPage() {
                 Unlock Chat
               </Button>
             </form>
-          </div>
+          </div> 
         )}
       </div>
     </div>
