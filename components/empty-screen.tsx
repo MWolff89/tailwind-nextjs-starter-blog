4e2ba313316f3a694bae0bb74e4774b74087f7f8
MWolff89 <150514371+MWolff89@users.noreply.github.com>
2024-02-10 13:25:45 +0000

    Pinecone Serverless

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1117,8 +1117,7 @@ type EmptyScreenProps = {
 }
 
 export function EmptyScreen({ setInput, index }: EmptyScreenProps) {
-  const { title, exampleMessages } =
-    indexToContent[index as keyof typeof indexToContent]
+  const { title, exampleMessages } = indexToContent[index as keyof typeof indexToContent]
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="bg-background rounded-lg border p-8">
